bb3146d56e5a7ce25c264a2fabb8987b013268f1
qxchuckle <1934009145@qq.com>
2024-06-01 20:45:31 +0800

    杂项

--- config.js
+++ config.js
@@ -1,4 +1,5 @@
 const config = {
+  // mongodb://root:123456@127.0.0.1:27017/paste?authSource=admin
   URL: process.env.MODB_URL || 'mongodb://127.0.0.1:27017/paste',
   // 加密字符串
   session_secret: 'chuckle',
